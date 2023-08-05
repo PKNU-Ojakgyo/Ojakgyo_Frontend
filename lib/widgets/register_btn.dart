100d9ca48c6cdc3270035f27a2722269fd343f99
huchujj <tjddk6662@naver.com>
2023-08-05 16:05:24 +0900

    style : 디자인 요구에 맞게 버튼 스타일 지정
    기획한 디자인 요구에 맞게 거래 등록 버튼 스타일을 수정
    
    issues : #21

--- lib/widgets/register_btn.dart
+++ lib/widgets/register_btn.dart
@@ -12,7 +12,22 @@ class RegisterBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () => {print('click')},
-      child: Text(btnName),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        backgroundColor: const Color(0xFF23225C),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          btnName,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
     );
   }
 }
