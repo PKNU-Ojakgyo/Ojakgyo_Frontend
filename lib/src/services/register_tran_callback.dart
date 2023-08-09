09aebea0447c65fa581879ba955a090b34e67370
huchujj <tjddk6662@naver.com>
2023-08-10 01:01:12 +0900

    feat : 거래 등록 정보를 받아 json으로 변환하는 class 생성
    거래 등록 페이지에서 입력 받은 데이터를 json으로 encode하는 callback
    class 생성
    
    issues : #39

---
+++ lib/src/services/register_tran_callback.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+class RegisterTranCallback {
+  static void registerTranCallback(
+    int lockerID,
+    String lockerAddress,
+    String name,
+    String cellPhone,
+    String account,
+    String accountBank,
+    int price,
+    String itemPrice,
+    String itemName,
+    String itemCondtion,
+    String buyerName,
+    String buyerCellPhone,
+  ) {
+    // 데이터를 JSON으로 직렬화
+    Map<String, dynamic> tranRegisterationData = {
+      "locker_id": lockerID,
+      "locker_address": lockerAddress,
+      "name": name,
+      "cellphone": cellPhone,
+      "account": account,
+      "account_back": accountBank,
+      "price": itemPrice,
+      "item": itemName,
+      "condition": itemCondtion,
+      "buyer_name": buyerName,
+      "buyer_cellphone": buyerCellPhone,
+    };
+
+    // 데이터를 JSON 문자열로 변환
+    String tranRegisterationJsonData = jsonEncode(tranRegisterationData);
+    print(tranRegisterationJsonData);
+  }
+}
