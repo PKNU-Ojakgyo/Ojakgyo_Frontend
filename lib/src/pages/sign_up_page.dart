cae46fdd45e98863bade2a0c65ded47d63fb2108
huchujj <tjddk6662@naver.com>
2023-09-02 21:30:10 +0900

    style : 중복확인 버튼 추가
    widgets/duplicate_btn.dart 추가
    회원가입 페이지에 중복확인 버튼 추가
    
    issues : #69

--- lib/src/pages/sign_up_page.dart
+++ lib/src/pages/sign_up_page.dart
@@ -3,9 +3,8 @@ import 'package:ojakgyo/widgets/back_navbar.dart';
 import 'package:ojakgyo/widgets/main_title.dart';
 import 'package:ojakgyo/widgets/line.dart';
 import 'package:ojakgyo/widgets/sub_title.dart';
-import 'package:ojakgyo/widgets/inquiry_btn.dart';
 import 'package:ojakgyo/widgets/text_input.dart';
-import 'package:ojakgyo/widgets/inquiry_counterparty_modal.dart';
+import 'package:ojakgyo/widgets/duplicate_btn.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -22,68 +21,112 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: const BackNavBar(),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(26),
-          child: Column(
-            children: [
-              const MainTitle(mainTitle: '회원가입'),
-              const Line(),
-              const SubTitle(subTitle: '아이디 입력'),
-              TextInput(
-                textType: '아이디',
-                hintText: '이메일을 입력하세요.',
-                isDefault: false,
-                controller: emailController,
+      body: Padding(
+        padding: const EdgeInsets.all(26),
+        child: Column(
+          children: [
+            const MainTitle(mainTitle: '회원가입'),
+            const Line(),
+            const SubTitle(subTitle: '아이디 입력'),
+            Row(
+              children: [
+                Expanded(
+                  flex: 24,
+                  child: TextInput(
+                    textType: '아이디',
+                    hintText: '이메일을 입력하세요.',
+                    isDefault: false,
+                    controller: emailController,
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                Expanded(
+                  flex: 11,
+                  child: DuplicateBtn(
+                    title: '중복확인',
+                    onPressed: () {
+                      print('클릭');
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const SubTitle(subTitle: '비밀번호 입력'),
+            TextInput(
+              textType: '비밀번호',
+              hintText: '비밀번호를 입력하세요.(영문, 숫자 조합으로 12자 이상)',
+              isDefault: false,
+              controller: passwordController,
+            ),
+            TextInput(
+              textType: '비밀번호 재확인',
+              hintText: '비밀번호를 다시 입력하세요.',
+              isDefault: false,
+              controller: rePasswordController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const SubTitle(subTitle: '이름 입력'),
+            TextInput(
+              textType: '이름',
+              hintText: '실명을 입력하세요.(반드시 실명을 입력)',
+              isDefault: false,
+              controller: passwordController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const SubTitle(subTitle: '전화번호 입력'),
+            Row(
+              children: [
+                Expanded(
+                  flex: 24,
+                  child: TextInput(
+                    textType: '전화번호',
+                    hintText: '전화번호를 입력하세요.',
+                    isDefault: false,
+                    controller: passwordController,
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                Expanded(
+                  flex: 11,
+                  child: DuplicateBtn(
+                    title: '중복확인',
+                    onPressed: () {
+                      print('클릭');
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 70,
+                    width: 150,
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SubTitle(subTitle: '비밀번호 입력'),
-              TextInput(
-                textType: '비밀번호',
-                hintText: '비밀번호를 입력하세요.(영문, 숫자 조합으로 12자 이상)',
-                isDefault: false,
-                controller: passwordController,
-              ),
-              TextInput(
-                textType: '비밀번호 재확인',
-                hintText: '비밀번호를 다시 입력하세요.',
-                isDefault: false,
-                controller: rePasswordController,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SubTitle(subTitle: '이름 입력'),
-              TextInput(
-                textType: '이름',
-                hintText: '실명을 입력하세요.(반드시 실명을 입력)',
-                isDefault: false,
-                controller: passwordController,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SubTitle(subTitle: '전화번호 입력'),
-              TextInput(
-                textType: '전회번호',
-                hintText: '전화번호를 입력하세요.',
-                isDefault: false,
-                controller: passwordController,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Image.asset(
-                'assets/images/logo.png',
-                height: 70,
-                width: 150,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
